c7a9a1f0969230ae6b76df062c9a44344a7db64d
Anil4563y <pndy0608@gmail.com>
2025-03-06 10:10:20 +0545

    new commit

---
+++ unit6.socket-for-client/DictClient.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+
+public class DictClient {
+    public static void main(String[] args) {
+        String server ="dict.org";
+        int port =2628;
+
+        try ( Socket socket = new Socket(server,port)){
+            socket.setSoTimeout(30000);
+
+
+            Writer writer = new OutputStreamWriter(socket.getOutputStream(),StandardCharsets.UTF_8);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(),StandardCharsets.UTF_8));
+            String  serverResponse = reader.readLine();
+            if(!serverResponse.startsWith("22o")){
+                System.err.println("Error:UNEXPECTED FROM SERVER :" + serverResponse);
+return ;
+
+            }
+
+            writer.write("Show DB\r\n");
+            writer.flush();
+
+            System.out.println("Available database:");
+String line;
+while((line = reader.readLine())!=null){
+    if(line.equals("."))
+    break;
+    System.out.println(line);
+
+}
+String word ="gold";
+writer.write("DEFINE fd-eng-lat" +word +"\r\n");
+writer.flush();
+boolean definitionFound = false;
+while((line=reader.readLine())!=null){
+    if(line.startsWith("532")){
+        System.out.println("No definition found for :" +word);
+        break;
+    }
+    if (line.equals(".")){
+        break;
+    }
+    if(line.matches("^\\d{5}.*")){
+        definitionFound =true;
+        System.out.println(line);
+
+    }
+
+    if(!definitionFound){
+        System.out.println("No valid defination returned.");
+
+    }
+    writer.write("quit\r\n");
+    writer.flush();
+    {
+}catch(SocketTimeoutException e){
+    System.out.println("Error:Server took too ling to respond ,Try agaiub kater .1");
+
+}catch(IOException e){
+    e.printStackTrace();
+}
+
+           
+        }
+    
+    
+
+
---
+++ unit6.socket-for-client/ReadFromServer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+public class ReadFromServer {
+    public static void main(String[] args) {
+
+        String somehost="example.com";
+        int someport=80;
+        try{
+            Socket socket =new Socket(somehost, someport);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream pstream = new PrintStream(socket.getOutputStream());
+            pstream.println("Get/HTTP/1.1");
+            pstream.println("Host:"+somehost);
+            pstream.println("Connection:close");
+            pstream.println();
+
+            String line;
+            while((line=reader.readline())!=null){
+                System.out.println(line);
+            }
+            socket.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
